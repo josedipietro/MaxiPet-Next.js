71cdfe8c9ed2c4bc8bd650cdc3b6b0615beb9544
José Di Pietro <dijose29@gmail.com>
2023-08-11 13:30:50 -0400

    Adding Icons

---
+++ lib/icons/index.ts
@@ -0,0 +1,4 @@
+export { FacebookIcon } from './FacebookIcon';
+export { TwitterIcon } from './TwitterIcon';
+export { InstagramIcon } from './InstagramIcon';
+export { LocationIcon } from './LocationIcon';
---
+++ lib/icons/types.d.ts
@@ -0,0 +1,4 @@
+export interface IconsProps {
+  size?: number;
+  color?: string;
+}
